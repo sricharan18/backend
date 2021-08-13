76251b3351b8e502aefbb42bf10a99029bf56e2e
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:51:24 -0700

    File commit

--- src/main/java/com/simplify/marketplace/domain/File.java
+++ src/main/java/com/simplify/marketplace/domain/File.java
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.simplify.marketplace.domain.enumeration.FileFormat;
 import com.simplify.marketplace.domain.enumeration.FileType;
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A File.
  */
 @Entity
 @Table(name = "file")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class File implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -66,22 +69,9 @@ public class File implements Serializable {
     )
     private Worker worker;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public File id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getPath() {
-        return this.path;
     }
 
     public File path(String path) {
@@ -89,25 +79,9 @@ public class File implements Serializable {
         return this;
     }
 
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public FileFormat getFileformat() {
-        return this.fileformat;
-    }
-
     public File fileformat(FileFormat fileformat) {
         this.fileformat = fileformat;
         return this;
-    }
-
-    public void setFileformat(FileFormat fileformat) {
-        this.fileformat = fileformat;
-    }
-
-    public FileType getFiletype() {
-        return this.filetype;
     }
 
     public File filetype(FileType filetype) {
@@ -115,25 +89,9 @@ public class File implements Serializable {
         return this;
     }
 
-    public void setFiletype(FileType filetype) {
-        this.filetype = filetype;
-    }
-
-    public String getTag() {
-        return this.tag;
-    }
-
     public File tag(String tag) {
         this.tag = tag;
         return this;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public Boolean getIsDefault() {
-        return this.isDefault;
     }
 
     public File isDefault(Boolean isDefault) {
@@ -141,25 +99,9 @@ public class File implements Serializable {
         return this;
     }
 
-    public void setIsDefault(Boolean isDefault) {
-        this.isDefault = isDefault;
-    }
-
-    public Boolean getIsResume() {
-        return this.isResume;
-    }
-
     public File isResume(Boolean isResume) {
         this.isResume = isResume;
         return this;
-    }
-
-    public void setIsResume(Boolean isResume) {
-        this.isResume = isResume;
-    }
-
-    public Boolean getIsProfilePic() {
-        return this.isProfilePic;
     }
 
     public File isProfilePic(Boolean isProfilePic) {
@@ -167,24 +109,9 @@ public class File implements Serializable {
         return this;
     }
 
-    public void setIsProfilePic(Boolean isProfilePic) {
-        this.isProfilePic = isProfilePic;
-    }
-
-    public Worker getWorker() {
-        return this.worker;
-    }
-
     public File worker(Worker worker) {
         this.setWorker(worker);
         return this;
-    }
-
-    public void setWorker(Worker worker) {
-        this.worker = worker;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
     }
 
     public File createdBy(String createdBy) {
@@ -192,25 +119,9 @@ public class File implements Serializable {
         return this;
     }
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
-    }
-
     public File createdAt(LocalDate createdAt) {
         this.createdAt = createdAt;
         return this;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
     }
 
     public File updatedBy(String updatedBy) {
@@ -218,58 +129,8 @@ public class File implements Serializable {
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public File updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof File)) {
-            return false;
-        }
-        return id != null && id.equals(((File) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "File{" +
-            "id=" + getId() +
-            ", path='" + getPath() + "'" +
-            ", fileformat='" + getFileformat() + "'" +
-            ", filetype='" + getFiletype() + "'" +
-            ", tag='" + getTag() + "'" +
-            ", isDefault='" + getIsDefault() + "'" +
-            ", isResume='" + getIsResume() + "'" +
-            ", isProfilePic='" + getIsProfilePic() + "'" +
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-            "}";
     }
 }
