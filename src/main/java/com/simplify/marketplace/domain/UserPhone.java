86d5a45fc99e3392922e0a95639fb3d55dd50437
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:42:52 -0700

    UserPhone Commit

--- src/main/java/com/simplify/marketplace/domain/UserPhone.java
+++ src/main/java/com/simplify/marketplace/domain/UserPhone.java
@@ -2,16 +2,19 @@ package com.simplify.marketplace.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A UserPhone.
  */
 @Entity
 @Table(name = "user_phone")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class UserPhone implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -48,22 +51,9 @@ public class UserPhone implements Serializable {
     @JsonIgnoreProperties(value = { "userEmails", "userPhones", "addresses" }, allowSetters = true)
     private CustomUser customUser;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public UserPhone id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getPhone() {
-        return this.phone;
     }
 
     public UserPhone phone(String phone) {
@@ -71,25 +61,9 @@ public class UserPhone implements Serializable {
         return this;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Boolean getIsActive() {
-        return this.isActive;
-    }
-
     public UserPhone isActive(Boolean isActive) {
         this.isActive = isActive;
         return this;
-    }
-
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
-
-    public Boolean getIsPrimary() {
-        return this.isPrimary;
     }
 
     public UserPhone isPrimary(Boolean isPrimary) {
@@ -97,25 +71,9 @@ public class UserPhone implements Serializable {
         return this;
     }
 
-    public void setIsPrimary(Boolean isPrimary) {
-        this.isPrimary = isPrimary;
-    }
-
-    public String getTag() {
-        return this.tag;
-    }
-
     public UserPhone tag(String tag) {
         this.tag = tag;
         return this;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public CustomUser getCustomUser() {
-        return this.customUser;
     }
 
     public UserPhone customUser(CustomUser customUser) {
@@ -123,24 +81,9 @@ public class UserPhone implements Serializable {
         return this;
     }
 
-    public void setCustomUser(CustomUser customUser) {
-        this.customUser = customUser;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
-    }
-
     public UserPhone createdBy(String createdBy) {
         this.createdBy = createdBy;
         return this;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
     }
 
     public UserPhone createdAt(LocalDate createdAt) {
@@ -148,70 +91,13 @@ public class UserPhone implements Serializable {
         return this;
     }
 
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
-    }
-
     public UserPhone updatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public UserPhone updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof UserPhone)) {
-            return false;
-        }
-        return id != null && id.equals(((UserPhone) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "UserPhone{" +
-            "id=" + getId() +
-            ", phone='" + getPhone() + "'" +
-            ", isActive='" + getIsActive() + "'" +
-            ", isPrimary='" + getIsPrimary() + "'" +
-            ", tag='" + getTag() + "'" +
-            
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-
-            "}";
     }
 }
