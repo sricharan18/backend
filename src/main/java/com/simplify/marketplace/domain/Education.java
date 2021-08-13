536545a1f1dd27a97b7dec7bf49fdc1103823b11
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:32:59 -0700

    education lombok

--- src/main/java/com/simplify/marketplace/domain/Education.java
+++ src/main/java/com/simplify/marketplace/domain/Education.java
@@ -6,16 +6,19 @@ import com.simplify.marketplace.domain.enumeration.EducationGrade;
 import com.simplify.marketplace.domain.enumeration.MarksType;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A Education.
  */
 @Entity
 @Table(name = "education")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class Education implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -71,6 +74,7 @@ public class Education implements Serializable {
 
     @Column(name = "updated_at")
     private LocalDate updatedAt;
+
     @ManyToOne
     private SubjectMaster majorSubject;
 
@@ -86,22 +90,9 @@ public class Education implements Serializable {
     )
     private Worker worker;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Education id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getDegreeName() {
-        return this.degreeName;
     }
 
     public Education degreeName(String degreeName) {
@@ -109,25 +100,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setDegreeName(String degreeName) {
-        this.degreeName = degreeName;
-    }
-
-    public String getInstitute() {
-        return this.institute;
-    }
-
     public Education institute(String institute) {
         this.institute = institute;
         return this;
-    }
-
-    public void setInstitute(String institute) {
-        this.institute = institute;
-    }
-
-    public Integer getYearOfPassing() {
-        return this.yearOfPassing;
     }
 
     public Education yearOfPassing(Integer yearOfPassing) {
@@ -135,25 +110,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setYearOfPassing(Integer yearOfPassing) {
-        this.yearOfPassing = yearOfPassing;
-    }
-
-    public Float getMarks() {
-        return this.marks;
-    }
-
     public Education marks(Float marks) {
         this.marks = marks;
         return this;
-    }
-
-    public void setMarks(Float marks) {
-        this.marks = marks;
-    }
-
-    public MarksType getMarksType() {
-        return this.marksType;
     }
 
     public Education marksType(MarksType marksType) {
@@ -161,25 +120,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setMarksType(MarksType marksType) {
-        this.marksType = marksType;
-    }
-
-    public EducationGrade getGrade() {
-        return this.grade;
-    }
-
     public Education grade(EducationGrade grade) {
         this.grade = grade;
         return this;
-    }
-
-    public void setGrade(EducationGrade grade) {
-        this.grade = grade;
-    }
-
-    public LocalDate getStartDate() {
-        return this.startDate;
     }
 
     public Education startDate(LocalDate startDate) {
@@ -187,25 +130,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return this.endDate;
-    }
-
     public Education endDate(LocalDate endDate) {
         this.endDate = endDate;
         return this;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public Boolean getIsComplete() {
-        return this.isComplete;
     }
 
     public Education isComplete(Boolean isComplete) {
@@ -213,25 +140,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setIsComplete(Boolean isComplete) {
-        this.isComplete = isComplete;
-    }
-
-    public DegreeType getDegreeType() {
-        return this.degreeType;
-    }
-
     public Education degreeType(DegreeType degreeType) {
         this.degreeType = degreeType;
         return this;
-    }
-
-    public void setDegreeType(DegreeType degreeType) {
-        this.degreeType = degreeType;
-    }
-
-    public String getDescription() {
-        return this.description;
     }
 
     public Education description(String description) {
@@ -239,25 +150,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public SubjectMaster getMajorSubject() {
-        return this.majorSubject;
-    }
-
     public Education majorSubject(SubjectMaster subjectMaster) {
         this.setMajorSubject(subjectMaster);
         return this;
-    }
-
-    public void setMajorSubject(SubjectMaster subjectMaster) {
-        this.majorSubject = subjectMaster;
-    }
-
-    public SubjectMaster getMinorSubject() {
-        return this.minorSubject;
     }
 
     public Education minorSubject(SubjectMaster subjectMaster) {
@@ -265,24 +160,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setMinorSubject(SubjectMaster subjectMaster) {
-        this.minorSubject = subjectMaster;
-    }
-
-    public Worker getWorker() {
-        return this.worker;
-    }
-
     public Education worker(Worker worker) {
         this.setWorker(worker);
         return this;
-    }
-
-    public void setWorker(Worker worker) {
-        this.worker = worker;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
     }
 
     public Education createdBy(String createdBy) {
@@ -290,25 +170,9 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
-    }
-
     public Education createdAt(LocalDate createdAt) {
         this.createdAt = createdAt;
         return this;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
     }
 
     public Education updatedBy(String updatedBy) {
@@ -316,62 +180,8 @@ public class Education implements Serializable {
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public Education updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Education)) {
-            return false;
-        }
-        return id != null && id.equals(((Education) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "Education{" +
-            "id=" + getId() +
-            ", degreeName='" + getDegreeName() + "'" +
-            ", institute='" + getInstitute() + "'" +
-            ", yearOfPassing=" + getYearOfPassing() +
-            ", marks=" + getMarks() +
-            ", marksType='" + getMarksType() + "'" +
-            ", grade='" + getGrade() + "'" +
-            ", startDate='" + getStartDate() + "'" +
-            ", endDate='" + getEndDate() + "'" +
-            ", isComplete='" + getIsComplete() + "'" +
-            ", degreeType='" + getDegreeType() + "'" +
-            ", description='" + getDescription() + "'" +
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-            "}";
     }
 }
