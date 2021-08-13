d9debc63119fc32b575fb87ca490fe32c75e2d15
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:43:29 -0700

    UserEmail Commit

--- src/main/java/com/simplify/marketplace/domain/UserEmail.java
+++ src/main/java/com/simplify/marketplace/domain/UserEmail.java
@@ -2,17 +2,20 @@ package com.simplify.marketplace.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.persistence.*;
 import javax.validation.constraints.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A UserEmail.
  */
 @Entity
 @Table(name = "user_email")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class UserEmail implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -51,22 +54,9 @@ public class UserEmail implements Serializable {
     @JsonIgnoreProperties(value = { "userEmails", "userPhones", "addresses" }, allowSetters = true)
     private CustomUser customUser;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public UserEmail id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getEmail() {
-        return this.email;
     }
 
     public UserEmail email(String email) {
@@ -74,25 +64,9 @@ public class UserEmail implements Serializable {
         return this;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Boolean getIsActive() {
-        return this.isActive;
-    }
-
     public UserEmail isActive(Boolean isActive) {
         this.isActive = isActive;
         return this;
-    }
-
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
-
-    public Boolean getIsPrimary() {
-        return this.isPrimary;
     }
 
     public UserEmail isPrimary(Boolean isPrimary) {
@@ -100,25 +74,9 @@ public class UserEmail implements Serializable {
         return this;
     }
 
-    public void setIsPrimary(Boolean isPrimary) {
-        this.isPrimary = isPrimary;
-    }
-
-    public String getTag() {
-        return this.tag;
-    }
-
     public UserEmail tag(String tag) {
         this.tag = tag;
         return this;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public CustomUser getCustomUser() {
-        return this.customUser;
     }
 
     public UserEmail customUser(CustomUser customUser) {
@@ -126,24 +84,9 @@ public class UserEmail implements Serializable {
         return this;
     }
 
-    public void setCustomUser(CustomUser customUser) {
-        this.customUser = customUser;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
-    }
-
     public UserEmail createdBy(String createdBy) {
         this.createdBy = createdBy;
         return this;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
     }
 
     public UserEmail createdAt(LocalDate createdAt) {
@@ -151,70 +94,13 @@ public class UserEmail implements Serializable {
         return this;
     }
 
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
-    }
-
     public UserEmail updatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public UserEmail updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof UserEmail)) {
-            return false;
-        }
-        return id != null && id.equals(((UserEmail) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "UserEmail{" +
-            "id=" + getId() +
-            ", email='" + getEmail() + "'" +
-            ", isActive='" + getIsActive() + "'" +
-            ", isPrimary='" + getIsPrimary() + "'" +
-            ", tag='" + getTag() + "'" +
-            
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-
-            "}";
     }
 }
