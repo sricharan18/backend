bc68df2e78bf9048b2d52cf44059b05c77791e52
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:44:50 -0700

    SubjectMaster Commit

--- src/main/java/com/simplify/marketplace/domain/SubjectMaster.java
+++ src/main/java/com/simplify/marketplace/domain/SubjectMaster.java
@@ -1,16 +1,19 @@
 package com.simplify.marketplace.domain;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A SubjectMaster.
  */
 @Entity
 @Table(name = "subject_master")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class SubjectMaster implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -34,22 +37,9 @@ public class SubjectMaster implements Serializable {
     @Column(name = "updated_at")
     private LocalDate updatedAt;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public SubjectMaster id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getSubjectName() {
-        return this.subjectName;
     }
 
     public SubjectMaster subjectName(String subjectName) {
@@ -57,24 +47,9 @@ public class SubjectMaster implements Serializable {
         return this;
     }
 
-    public void setSubjectName(String subjectName) {
-        this.subjectName = subjectName;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
-    }
-
     public SubjectMaster createdBy(String createdBy) {
         this.createdBy = createdBy;
         return this;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
     }
 
     public SubjectMaster createdAt(LocalDate createdAt) {
@@ -82,67 +57,13 @@ public class SubjectMaster implements Serializable {
         return this;
     }
 
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
-    }
-
     public SubjectMaster updatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public SubjectMaster updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof SubjectMaster)) {
-            return false;
-        }
-        return id != null && id.equals(((SubjectMaster) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "SubjectMaster{" +
-            "id=" + getId() +
-            ", subjectName='" + getSubjectName() + "'" +
-            
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-
-            "}";
     }
 }
