5bb2aa28d8e83c4bb5e6b135a30641448343ab5f
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:47:37 -0700

    Portfolio commit

--- src/main/java/com/simplify/marketplace/domain/Portfolio.java
+++ src/main/java/com/simplify/marketplace/domain/Portfolio.java
@@ -3,16 +3,19 @@ package com.simplify.marketplace.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.simplify.marketplace.domain.enumeration.PortfolioType;
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A Portfolio.
  */
 @Entity
 @Table(name = "portfolio")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class Portfolio implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -49,22 +52,9 @@ public class Portfolio implements Serializable {
     )
     private Worker worker;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Portfolio id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getPortfolioURL() {
-        return this.portfolioURL;
     }
 
     public Portfolio portfolioURL(String portfolioURL) {
@@ -72,37 +62,14 @@ public class Portfolio implements Serializable {
         return this;
     }
 
-    public void setPortfolioURL(String portfolioURL) {
-        this.portfolioURL = portfolioURL;
-    }
-
-    public PortfolioType getType() {
-        return this.type;
-    }
-
     public Portfolio type(PortfolioType type) {
         this.type = type;
         return this;
     }
 
-    public void setType(PortfolioType type) {
-        this.type = type;
-    }
-
-    public Worker getWorker() {
-        return this.worker;
-    }
-
     public Portfolio worker(Worker worker) {
         this.setWorker(worker);
         return this;
-    }
-
-    public void setWorker(Worker worker) {
-        this.worker = worker;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
     }
 
     public Portfolio createdBy(String createdBy) {
@@ -123,68 +90,13 @@ public class Portfolio implements Serializable {
         return this;
     }
 
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
-    }
-
     public Portfolio updatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public Portfolio updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Portfolio)) {
-            return false;
-        }
-        return id != null && id.equals(((Portfolio) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "Portfolio{" +
-            "id=" + getId() +
-            ", portfolioURL='" + getPortfolioURL() + "'" +
-            ", type='" + getType() + "'" +
-            
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-
-            "}";
     }
 }
