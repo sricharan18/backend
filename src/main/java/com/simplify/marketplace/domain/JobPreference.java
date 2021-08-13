d6aa7791ba0905ca4e7844c7ca0133e3893d6f4f
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:50:53 -0700

    JobPreference commit

--- src/main/java/com/simplify/marketplace/domain/JobPreference.java
+++ src/main/java/com/simplify/marketplace/domain/JobPreference.java
@@ -6,18 +6,21 @@ import com.simplify.marketplace.domain.enumeration.EngagementType;
 import com.simplify.marketplace.domain.enumeration.LocationType;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A JobPreference.
  */
 @Entity
 @Table(name = "job_preference")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class JobPreference implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -97,22 +100,9 @@ public class JobPreference implements Serializable {
     )
     private Worker worker;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public JobPreference id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public Integer getHourlyRate() {
-        return this.hourlyRate;
     }
 
     public JobPreference hourlyRate(Integer hourlyRate) {
@@ -120,25 +110,9 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setHourlyRate(Integer hourlyRate) {
-        this.hourlyRate = hourlyRate;
-    }
-
-    public Integer getDailyRate() {
-        return this.dailyRate;
-    }
-
     public JobPreference dailyRate(Integer dailyRate) {
         this.dailyRate = dailyRate;
         return this;
-    }
-
-    public void setDailyRate(Integer dailyRate) {
-        this.dailyRate = dailyRate;
-    }
-
-    public Integer getMonthlyRate() {
-        return this.monthlyRate;
     }
 
     public JobPreference monthlyRate(Integer monthlyRate) {
@@ -146,25 +120,9 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setMonthlyRate(Integer monthlyRate) {
-        this.monthlyRate = monthlyRate;
-    }
-
-    public Integer getHourPerDay() {
-        return this.hourPerDay;
-    }
-
     public JobPreference hourPerDay(Integer hourPerDay) {
         this.hourPerDay = hourPerDay;
         return this;
-    }
-
-    public void setHourPerDay(Integer hourPerDay) {
-        this.hourPerDay = hourPerDay;
-    }
-
-    public Integer getHourPerWeek() {
-        return this.hourPerWeek;
     }
 
     public JobPreference hourPerWeek(Integer hourPerWeek) {
@@ -172,25 +130,9 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setHourPerWeek(Integer hourPerWeek) {
-        this.hourPerWeek = hourPerWeek;
-    }
-
-    public EngagementType getEngagementType() {
-        return this.engagementType;
-    }
-
     public JobPreference engagementType(EngagementType engagementType) {
         this.engagementType = engagementType;
         return this;
-    }
-
-    public void setEngagementType(EngagementType engagementType) {
-        this.engagementType = engagementType;
-    }
-
-    public EmploymentType getEmploymentType() {
-        return this.employmentType;
     }
 
     public JobPreference employmentType(EmploymentType employmentType) {
@@ -198,25 +140,9 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setEmploymentType(EmploymentType employmentType) {
-        this.employmentType = employmentType;
-    }
-
-    public LocationType getLocationType() {
-        return this.locationType;
-    }
-
     public JobPreference locationType(LocationType locationType) {
         this.locationType = locationType;
         return this;
-    }
-
-    public void setLocationType(LocationType locationType) {
-        this.locationType = locationType;
-    }
-
-    public LocalDate getAvailableFrom() {
-        return this.availableFrom;
     }
 
     public JobPreference availableFrom(LocalDate availableFrom) {
@@ -224,38 +150,14 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setAvailableFrom(LocalDate availableFrom) {
-        this.availableFrom = availableFrom;
-    }
-
-    public LocalDate getAvailableTo() {
-        return this.availableTo;
-    }
-
     public JobPreference availableTo(LocalDate availableTo) {
         this.availableTo = availableTo;
         return this;
     }
 
-    public void setAvailableTo(LocalDate availableTo) {
-        this.availableTo = availableTo;
-    }
-
-    public Boolean getIsActive() {
-        return this.isActive;
-    }
-
     public JobPreference isActive(Boolean isActive) {
         this.isActive = isActive;
         return this;
-    }
-
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
-
-    public Set<LocationPrefrence> getLocationPrefrences() {
-        return this.locationPrefrences;
     }
 
     public JobPreference locationPrefrences(Set<LocationPrefrence> locationPrefrences) {
@@ -275,20 +177,6 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setLocationPrefrences(Set<LocationPrefrence> locationPrefrences) {
-        if (this.locationPrefrences != null) {
-            this.locationPrefrences.forEach(i -> i.setWorker(null));
-        }
-        if (locationPrefrences != null) {
-            locationPrefrences.forEach(i -> i.setWorker(this));
-        }
-        this.locationPrefrences = locationPrefrences;
-    }
-
-    public Set<FieldValue> getFieldValues() {
-        return this.fieldValues;
-    }
-
     public JobPreference fieldValues(Set<FieldValue> fieldValues) {
         this.setFieldValues(fieldValues);
         return this;
@@ -306,31 +194,9 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setFieldValues(Set<FieldValue> fieldValues) {
-        if (this.fieldValues != null) {
-            this.fieldValues.forEach(i -> i.setJobpreference(null));
-        }
-        if (fieldValues != null) {
-            fieldValues.forEach(i -> i.setJobpreference(this));
-        }
-        this.fieldValues = fieldValues;
-    }
-
-    public Category getSubCategory() {
-        return this.subCategory;
-    }
-
     public JobPreference subCategory(Category category) {
         this.setSubCategory(category);
         return this;
-    }
-
-    public void setSubCategory(Category category) {
-        this.subCategory = category;
-    }
-
-    public Worker getWorker() {
-        return this.worker;
     }
 
     public JobPreference worker(Worker worker) {
@@ -338,24 +204,9 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setWorker(Worker worker) {
-        this.worker = worker;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
-    }
-
     public JobPreference createdBy(String createdBy) {
         this.createdBy = createdBy;
         return this;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
     }
 
     public JobPreference createdAt(LocalDate createdAt) {
@@ -363,75 +214,13 @@ public class JobPreference implements Serializable {
         return this;
     }
 
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
-    }
-
     public JobPreference updatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public JobPreference updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof JobPreference)) {
-            return false;
-        }
-        return id != null && id.equals(((JobPreference) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "JobPreference{" +
-            "id=" + getId() +
-            ", hourlyRate=" + getHourlyRate() +
-            ", dailyRate=" + getDailyRate() +
-            ", monthlyRate=" + getMonthlyRate() +
-            ", hourPerDay=" + getHourPerDay() +
-            ", hourPerWeek=" + getHourPerWeek() +
-            ", engagementType='" + getEngagementType() + "'" +
-            ", employmentType='" + getEmploymentType() + "'" +
-            ", locationType='" + getLocationType() + "'" +
-            ", availableFrom='" + getAvailableFrom() + "'" +
-            ", availableTo='" + getAvailableTo() + "'" +
-            ", isActive='" + getIsActive() + "'" +
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-            "}";
     }
 }
