83d8ab538498efde20faab2220f62471d7843c8f
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:35:23 -0700

    client lombok

--- src/main/java/com/simplify/marketplace/domain/Client.java
+++ src/main/java/com/simplify/marketplace/domain/Client.java
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.simplify.marketplace.domain.enumeration.CompanyType;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A Client.
  */
 @Entity
 @Table(name = "client")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class Client implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -61,22 +64,9 @@ public class Client implements Serializable {
     @JoinColumn(unique = true)
     private CustomUser customUser;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Client id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getCompanyName() {
-        return this.companyName;
     }
 
     public Client companyName(String companyName) {
@@ -84,25 +74,9 @@ public class Client implements Serializable {
         return this;
     }
 
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getCompanyWebsite() {
-        return this.companyWebsite;
-    }
-
     public Client companyWebsite(String companyWebsite) {
         this.companyWebsite = companyWebsite;
         return this;
-    }
-
-    public void setCompanyWebsite(String companyWebsite) {
-        this.companyWebsite = companyWebsite;
-    }
-
-    public CompanyType getCompanyType() {
-        return this.companyType;
     }
 
     public Client companyType(CompanyType companyType) {
@@ -110,25 +84,9 @@ public class Client implements Serializable {
         return this;
     }
 
-    public void setCompanyType(CompanyType companyType) {
-        this.companyType = companyType;
-    }
-
-    public String getPrimaryPhone() {
-        return this.primaryPhone;
-    }
-
     public Client primaryPhone(String primaryPhone) {
         this.primaryPhone = primaryPhone;
         return this;
-    }
-
-    public void setPrimaryPhone(String primaryPhone) {
-        this.primaryPhone = primaryPhone;
-    }
-
-    public Boolean getIsActive() {
-        return this.isActive;
     }
 
     public Client isActive(Boolean isActive) {
@@ -136,25 +94,9 @@ public class Client implements Serializable {
         return this;
     }
 
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
     public Client description(String description) {
         this.description = description;
         return this;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getStartDate() {
-        return this.startDate;
     }
 
     public Client startDate(LocalDate startDate) {
@@ -162,24 +104,9 @@ public class Client implements Serializable {
         return this;
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public CustomUser getCustomUser() {
-        return this.customUser;
-    }
-
     public Client customUser(CustomUser customUser) {
         this.setCustomUser(customUser);
         return this;
-    }
-
-    public void setCustomUser(CustomUser customUser) {
-        this.customUser = customUser;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
     }
 
     public Client createdBy(String createdBy) {
@@ -187,25 +114,9 @@ public class Client implements Serializable {
         return this;
     }
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
-    }
-
     public Client createdAt(LocalDate createdAt) {
         this.createdAt = createdAt;
         return this;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
     }
 
     public Client updatedBy(String updatedBy) {
@@ -213,58 +124,8 @@ public class Client implements Serializable {
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public Client updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Client)) {
-            return false;
-        }
-        return id != null && id.equals(((Client) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "Client{" +
-            "id=" + getId() +
-            ", companyName='" + getCompanyName() + "'" +
-            ", companyWebsite='" + getCompanyWebsite() + "'" +
-            ", companyType='" + getCompanyType() + "'" +
-            ", primaryPhone='" + getPrimaryPhone() + "'" +
-            ", isActive='" + getIsActive() + "'" +
-            ", description='" + getDescription() + "'" +
-            ", startDate='" + getStartDate() + "'" +
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-            "}";
     }
 }
