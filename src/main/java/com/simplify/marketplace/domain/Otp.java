605e339c3fcd1e8fe6373b1b302bd11e127c4d4a
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:49:04 -0700

    Otp commit

--- src/main/java/com/simplify/marketplace/domain/Otp.java
+++ src/main/java/com/simplify/marketplace/domain/Otp.java
@@ -4,16 +4,19 @@ import com.simplify.marketplace.domain.enumeration.OtpStatus;
 import com.simplify.marketplace.domain.enumeration.OtpType;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A Otp.
  */
 @Entity
 @Table(name = "otp")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class Otp implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -54,22 +57,9 @@ public class Otp implements Serializable {
     @Column(name = "status")
     private OtpStatus status;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Otp id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getContextId() {
-        return this.contextId;
     }
 
     public Otp contextId(String contextId) {
@@ -77,25 +67,9 @@ public class Otp implements Serializable {
         return this;
     }
 
-    public void setContextId(String contextId) {
-        this.contextId = contextId;
-    }
-
-    public Integer getOtp() {
-        return this.otp;
-    }
-
     public Otp otp(Integer otp) {
         this.otp = otp;
         return this;
-    }
-
-    public void setOtp(Integer otp) {
-        this.otp = otp;
-    }
-
-    public String getEmail() {
-        return this.email;
     }
 
     public Otp email(String email) {
@@ -103,25 +77,9 @@ public class Otp implements Serializable {
         return this;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Boolean getIsActive() {
-        return this.isActive;
-    }
-
     public Otp isActive(Boolean isActive) {
         this.isActive = isActive;
         return this;
-    }
-
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
-
-    public Integer getPhone() {
-        return this.phone;
     }
 
     public Otp phone(Integer phone) {
@@ -129,25 +87,9 @@ public class Otp implements Serializable {
         return this;
     }
 
-    public void setPhone(Integer phone) {
-        this.phone = phone;
-    }
-
-    public OtpType getType() {
-        return this.type;
-    }
-
     public Otp type(OtpType type) {
         this.type = type;
         return this;
-    }
-
-    public void setType(OtpType type) {
-        this.type = type;
-    }
-
-    public LocalDate getExpiryTime() {
-        return this.expiryTime;
     }
 
     public Otp expiryTime(LocalDate expiryTime) {
@@ -155,24 +97,9 @@ public class Otp implements Serializable {
         return this;
     }
 
-    public void setExpiryTime(LocalDate expiryTime) {
-        this.expiryTime = expiryTime;
-    }
-
-    public OtpStatus getStatus() {
-        return this.status;
-    }
-
     public Otp status(OtpStatus status) {
         this.status = status;
         return this;
-    }
-
-    public void setStatus(OtpStatus status) {
-        this.status = status;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
     }
 
     public Otp createdBy(String createdBy) {
@@ -180,58 +107,8 @@ public class Otp implements Serializable {
         return this;
     }
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
-    }
-
     public Otp createdAt(LocalDate createdAt) {
         this.createdAt = createdAt;
         return this;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
- 
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Otp)) {
-            return false;
-        }
-        return id != null && id.equals(((Otp) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "Otp{" +
-            "id=" + getId() +
-            ", contextId='" + getContextId() + "'" +
-            ", otp=" + getOtp() +
-            ", email='" + getEmail() + "'" +
-            ", isActive='" + getIsActive() + "'" +
-            ", phone=" + getPhone() +
-            ", type='" + getType() + "'" +
-            ", expiryTime='" + getExpiryTime() + "'" +
-            ", status='" + getStatus() + "'" +
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            "}";
     }
 }
