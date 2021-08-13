b967964ba6d32e0261e77faff1718b6c5c5c0ad7
prathyusha2511 <prathyushanandikonda@gmail.com>
2021-08-13 16:53:44 -0700

    Employment commit

--- src/main/java/com/simplify/marketplace/domain/Employment.java
+++ src/main/java/com/simplify/marketplace/domain/Employment.java
@@ -3,18 +3,21 @@ package com.simplify.marketplace.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import java.time.LocalDate;
+
 /**
  * A Employment.
  */
 @Entity
 @Table(name = "employment")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class Employment implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -74,22 +77,9 @@ public class Employment implements Serializable {
     )
     private Worker worker;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Employment id(Long id) {
         this.id = id;
         return this;
-    }
-
-    public String getJobTitle() {
-        return this.jobTitle;
     }
 
     public Employment jobTitle(String jobTitle) {
@@ -97,25 +87,9 @@ public class Employment implements Serializable {
         return this;
     }
 
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public String getCompanyName() {
-        return this.companyName;
-    }
-
     public Employment companyName(String companyName) {
         this.companyName = companyName;
         return this;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public LocalDate getStartDate() {
-        return this.startDate;
     }
 
     public Employment startDate(LocalDate startDate) {
@@ -123,25 +97,9 @@ public class Employment implements Serializable {
         return this;
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return this.endDate;
-    }
-
     public Employment endDate(LocalDate endDate) {
         this.endDate = endDate;
         return this;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public Boolean getIsCurrent() {
-        return this.isCurrent;
     }
 
     public Employment isCurrent(Boolean isCurrent) {
@@ -149,38 +107,14 @@ public class Employment implements Serializable {
         return this;
     }
 
-    public void setIsCurrent(Boolean isCurrent) {
-        this.isCurrent = isCurrent;
-    }
-
-    public Integer getLastSalary() {
-        return this.lastSalary;
-    }
-
     public Employment lastSalary(Integer lastSalary) {
         this.lastSalary = lastSalary;
         return this;
     }
 
-    public void setLastSalary(Integer lastSalary) {
-        this.lastSalary = lastSalary;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
     public Employment description(String description) {
         this.description = description;
         return this;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Location> getLocations() {
-        return this.locations;
     }
 
     public Employment locations(Set<Location> locations) {
@@ -200,31 +134,9 @@ public class Employment implements Serializable {
         return this;
     }
 
-    public void setLocations(Set<Location> locations) {
-        if (this.locations != null) {
-            this.locations.forEach(i -> i.setEmployment(null));
-        }
-        if (locations != null) {
-            locations.forEach(i -> i.setEmployment(this));
-        }
-        this.locations = locations;
-    }
-
-    public Client getCompany() {
-        return this.company;
-    }
-
     public Employment company(Client client) {
         this.setCompany(client);
         return this;
-    }
-
-    public void setCompany(Client client) {
-        this.company = client;
-    }
-
-    public Worker getWorker() {
-        return this.worker;
     }
 
     public Employment worker(Worker worker) {
@@ -232,24 +144,9 @@ public class Employment implements Serializable {
         return this;
     }
 
-    public void setWorker(Worker worker) {
-        this.worker = worker;
-    }
-    public String getCreatedBy() {
-        return this.createdBy;
-    }
-
     public Employment createdBy(String createdBy) {
         this.createdBy = createdBy;
         return this;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDate getCreatedAt() {
-        return this.createdAt;
     }
 
     public Employment createdAt(LocalDate createdAt) {
@@ -257,71 +154,13 @@ public class Employment implements Serializable {
         return this;
     }
 
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedBy() {
-        return this.updatedBy;
-    }
-
     public Employment updatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
         return this;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return this.updatedAt;
-    }
-
     public Employment updatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
         return this;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }    
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Employment)) {
-            return false;
-        }
-        return id != null && id.equals(((Employment) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "Employment{" +
-            "id=" + getId() +
-            ", jobTitle='" + getJobTitle() + "'" +
-            ", companyName='" + getCompanyName() + "'" +
-            ", startDate='" + getStartDate() + "'" +
-            ", endDate='" + getEndDate() + "'" +
-            ", isCurrent='" + getIsCurrent() + "'" +
-            ", lastSalary=" + getLastSalary() +
-            ", description='" + getDescription() + "'" +
-            ", createdBy='" + getCreatedBy() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", updatedBy='" + getUpdatedBy() + "'" +
-            ", updatedAt='" + getUpdatedAt() + "'" +
-            "}";
     }
 }
